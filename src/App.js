150900896d3c0f59fdafb7fb0445085331d9eea4
ghadamo <ghadamo814@gmail.com>
2022-06-26 00:57:35 +0200

    react memory game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cards from "./components/Cards";
+
+function App (){
+  return(
+    <div className="App">
+      <h1 className="title">Memory Game With React</h1>
+      <Cards />
+    {/* {<div className="container"></div>} */}
+    </div>
+  )
+}
+export default App;
